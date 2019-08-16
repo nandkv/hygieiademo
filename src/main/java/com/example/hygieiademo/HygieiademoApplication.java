e74ddc0172f76f8aa9ba3156d12fe682fba73735
nandkv <nandkv.online@gmail.com>
2019-08-15 22:32:21 -0500

    Update HygieiademoApplication.java

--- src/main/java/com/example/hygieiademo/HygieiademoApplication.java
+++ src/main/java/com/example/hygieiademo/HygieiademoApplication.java
@@ -8,8 +8,6 @@ public class HygieiademoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HygieiademoApplication.class, args);
-		//Inline comments
-		System.out.println("Testing Sonarqube");
 	}
 
 }

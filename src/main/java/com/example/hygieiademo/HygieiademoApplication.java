0ba304219b765fafead92e3c417e2f5218343799
nand <nandkv.online@gmail.com>
2019-08-15 21:12:00 -0500

    first commit

---
+++ src/main/java/com/example/hygieiademo/HygieiademoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hygieiademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HygieiademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HygieiademoApplication.class, args);
+	}
+
+}
